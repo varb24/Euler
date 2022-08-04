063918bd6bd771cba03165bcd5e759356ff3aa57
varb24 <39485630+varb24@users.noreply.github.com>
2022-08-04 01:38:11 -0400

    Add files via upload
    Finds triangle number with n multiples.

--- ProjectEuler/src/euler12/NthTriangleNum.java
+++ ProjectEuler/src/euler12/NthTriangleNum.java
@@ -1,25 +1,67 @@
 package euler12;
+import java.util.ArrayList;
 import java.util.LinkedList;
+
 import java.util.Queue;
-
 import projectEuler.FindingPrimes;
-
+//76588876 is wrong
 public class NthTriangleNum {
-
+	
+	//Write Function to find the factors of a number, this is the same as finding the prime number composites of a number.
 	public static void main(String[] args) {
-		boolean notFound = true;
-		int numOfDivisors = 0;
-		int nthTriangle = 1;
-			
-		//while(notFound) {
-				
-			//}
-		Queue<String> a = new LinkedList<String>();
-		a.add("hi");
-		System.out.print(a.remove());
+		int numOfMults = 500; 
+		int factors = 0;
+		int triangleTest = 1;
+		
+		System.out.println((findNMul2(triangleNum(20))));
+		
+		/*while(factors <numOfMults) {
+			factors = findNMul2(triangleNum(triangleTest));
+			triangleTest++;
+		}
+		triangleTest--;
+		System.out.println(triangleNum(triangleTest));
+			*/
 	}
 	
-	static double triangleNum(int n) { //finds nth triangle number
+	static int triangleNum(int n) { //finds nth triangle number
 		return n*(n+1)/2;
 	}
+	static int findNMul2(int tn) {
+		int mults = 2; //Starts at 2 to include 1 and the number itself
+		int firstM= 2;
+		int secondM = 3;
+
+
+				while(firstM< secondM) {
+					//System.out.println(firstM + " " + secondM);
+					if (tn%firstM==0) {
+						secondM = tn/firstM;
+						firstM+=1;
+						mults+=2;
+						System.out.println(" mults added " + secondM);
+					}
+					else firstM++;												
+			}
+			System.out.println("returning mults : " + mults + " for tn: " + tn);
+		return mults;		
+	}
+	/*static int findNMul(int tn) {
+		int mults = 2; //Starts at 2 to include 1 and the number itself
+		int newtn = tn;
+		PrimeArrays multiples = new PrimeArrays(tn); //Creates primearr w/ numbers up to tn
+		ArrayList<Integer> multList = multiples.getPrimeArray();
+		System.out.println(multList);
+		
+		for(int prime:multList){
+			if (tn%prime==0) {
+
+					System.out.println("Added the result of:" +tn +" prime: " + prime);
+					mults+=2;
+				}
+			
+		}
+				
+		return mults;
+	} prime arrays is on my laptop*/
 }
